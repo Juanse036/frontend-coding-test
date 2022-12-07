e2c1c7adb7f4738cb27d09f6cc9eb16962606970
Juanse036 <juansebas036@gmail.com>
2022-12-07 16:31:54 -0500

    Update people.js
    Add Update Data function

--- actions/people.js
+++ actions/people.js
@@ -8,11 +8,23 @@ class People {
     }
 
     async getPeopleById(Id){        
-        const response = await fetch(`http://localhost:3001/people/${Id}`)
-        const PeopleData = await response.json();         
+        const response = await fetch(`http://localhost:3001/people?id=${Id}`)
+        const PeopleData = await response.json();                 
         return PeopleData  
     }
-    
+
+    async updatePeopleDataById(Id, Data){
+        console.log({Id, Data})
+        const response = await fetch(`http://localhost:3001/people/${Id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            // body: '{"name": "Lisa", "salary": "8000"}',
+            body: JSON.stringify(Data)
+        });
+        console.log({response})
+    }
 }
 
 export default new People()
