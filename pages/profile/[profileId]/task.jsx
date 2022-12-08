2005a30c493e457907203688bbab89434194f9ac
Juanse036 <juansebas036@gmail.com>
2022-12-08 15:33:44 -0500

    Functionality

--- pages/profile/[profileId]/task.jsx
+++ pages/profile/[profileId]/task.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import task from '../../../actions/task'
 
 import tasks from '../../../actions/task'
 
@@ -7,15 +8,21 @@ const Task = () => {
 
     const router = useRouter()
     const profileId = router.query.profileId
-    
-    
 
     const [loading, setLoading] = useState(true)
-    const [tasksData, setTasksData] = useState([{}])        
+    const [tasksData, setTasksData] = useState([{        
+        title: '',
+        description: '',
+        completed: false,
+        startDate:'',
+        endDate:null,
+        personId: ''
+    }])     
     
     const getTasksData = async(profileId) => {
-        const data = await tasks.getTaskById(profileId)                 
-        setTasksData(data);         
+        const data = await tasks.getTaskByProfileId(profileId)                 
+        setTasksData(data);      
+
     }
 
     useEffect(() => {
@@ -26,12 +33,34 @@ const Task = () => {
         if(profileId) getData()
     }, [profileId])
 
-    const handleTaskCompleted = () => {
+    const handleInputChange = async(e) => {
+        e.preventDefault()
+        
+        const { name, value, id } = e.target;
+        const currentTaskDataArray = await task.getTaskById(id)
+        let currentTaskData = currentTaskDataArray[0]
+        
 
-    }
+        if (value == "true") {
+            currentTaskData.completed = false
+            currentTaskData.endDate = null
+
+        }else {
+            currentTaskData.completed = true            
+        }       
+
+        await tasks.updateTaskById(id, currentTaskData)    
+        getTasksData(profileId);
+       }; 
 
 
-    if (loading) return <p>Loading...</p>
+       if (loading) return (
+        <div className='w-100 min-vh-100 d-flex justify-content-center align-items-center'>
+            <div class="spinner-border" role="status">
+                <span class="sr-only"></span>
+            </div>
+        </div>
+    )
 
 
     return(
@@ -39,7 +68,7 @@ const Task = () => {
             <h1 className='text-center mb-4'>TASKS</h1>
         {tasksData.map(el => {
             return(
-                    <div className='d-flex justify-content-center'>
+                    <div className='d-flex justify-content-center' key={el.id}>
                         <div className="list-group w-75">
                             <a href="#" className={`list-group-item list-group-item-action ${el.completed ? 'list-group-item-success': 'list-group-item-light'} `}>
                                 <div className="d-flex w-100 justify-content-between">
@@ -52,13 +81,13 @@ const Task = () => {
                                 <div className="d-flex w-100 justify-content-between">
                                     <p className="mb-1">{el.description}</p>
                                     <div className="form-check ">
-                                        <input className={`form-check-input`} type="checkbox" value="" checked={el.completed}/>
+                                        <input className={`form-check-input`} type="checkbox" name="completed" value={el.completed} id={el.id} checked={el.completed} onChange={handleInputChange}/>
                                         <label className="form-check-label" for="flexCheckDefault">
                                             {el.completed ? 'Mark as not completed' : 'Mark as completed'}
                                         </label>
                                     </div>                                    
                                 </div>
-                                <button type="button" class="btn btn-dark mt-3">Edit</button>                                
+                                <button type="button" className="btn btn-dark mt-3" onClick={() => router.push(`/tasks/${el.id}/edit`)}>Edit</button> 
                             </a>
                         </div>
                     </div>
